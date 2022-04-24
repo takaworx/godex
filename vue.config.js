fe4eaed22d5f070be14c6ba3e5d5a67ad978bf21
takaworx <neil@takaworx.com>
2022-04-25 00:17:35 +0800

    added vuetify

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,9 @@ const { defineConfig } = require('@vue/cli-service')
 const path = require('path')
 
 module.exports = defineConfig({
-  transpileDependencies: true,
+  transpileDependencies: [
+    'vuetify'
+  ],
   configureWebpack: {
     resolve: {
       alias: {
