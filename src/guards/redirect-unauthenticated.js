ecb2c45f52c9f2b7920f28b9f9eb2644535a65ab
takaworx <neil@takaworx.com>
2022-04-27 20:59:06 +0800

    save user to store in guard

--- src/guards/redirect-unauthenticated.js
+++ src/guards/redirect-unauthenticated.js
@@ -2,11 +2,14 @@ import Vue from 'vue'
 import storage from '@/abstract/storage'
 import UserService from '@/services/user'
 
+import store from '@/store/index'
+
 export default async function (to, from, next) {
   try {
     const token = await storage.getItem('token')
     Vue.axios.defaults.headers.common.Authorization = `Bearer ${token}`
-    await UserService.getUser()
+    const user = await UserService.getUser()
+    store.commit('setUser', user.data)
   } catch (err) {
     console.error(err)
     next({ name: 'login' })
